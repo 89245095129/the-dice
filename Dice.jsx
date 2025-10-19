81bd9c0551ac068cb149b1fb18ea37181294de87
Светлана <lana.tyumentseva.1970@mail.ru>
2025-10-19 18:11:51 +0900

    Dice.jsx

--- Dice.jsx
+++ Dice.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
+import dice1 from '../assets/dice1.png';
+import dice2 from '../assets/dice2.png';
+import dice3 from '../assets/dice3.png';
+import dice4 from '../assets/dice4.png';
+import dice5 from '../assets/dice5.png';
+import dice6 from '../assets/dice6.png';
 
 function Dice({ value }) {
-  const diceImages = [
-    require('../assets/dice1.png'),
-    require('../assets/dice2.png'),
-    require('../assets/dice3.png'),
-    require('../assets/dice4.png'),
-    require('../assets/dice5.png'),
-    require('../assets/dice6.png'),
-  ];
+  const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
 
   return (
     <img
       src={diceImages[value - 1]}
       alt={`Выпало: ${value}`}
       className="dice-image"
-      key={value} // Добавляем ключ для принудительной перерисовки
+      key={value}
     />
   );
 }
